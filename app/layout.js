03b1955c36336f3eac9e177f2ee37b2a2200db69
Ansul Agrawal <ansulagrawal9@gmail.com>
2024-05-05 14:45:56 +0530

    Added Site Config

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,25 @@
+import { siteConfig } from '@/config/site';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Jira',
-  description: '',
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  authors: siteConfig.authors,
+  creator: siteConfig.creator,
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
 };
 
 export default function RootLayout({ children }) {
---
+++ config/site.js
@@ -0,0 +1,21 @@
+const siteConfig = {
+  name: 'Jira Clone 🚀',
+  description: 'An open source Jira clone built with Next.js 14, featuring React server components. Powered by tailwind, shadcn. Efficient project management at your fingertips.',
+  keywords: ['Jira', 'Next.js', 'React', 'Tailwind CSS', 'Server Components', 'shadcn'],
+  authors: [
+    {
+      name: 'Ansul Agrawal',
+      url: 'https://ansulagrawal.vercel.app/',
+    },
+    {
+      name: 'Jitendra Soni',
+      url: 'https://github.com/JitendraSoni1234',
+    },
+  ],
+  links: {
+    github: 'https://github.com/ansulagrawal/jira-clone',
+  },
+  creator: 'Ansul Agrawal, Jitendra Soni',
+};
+
+export { siteConfig };
