1b7df1611744ee196bc4ddb3318360037fb42ae3
Ansul Agrawal <ansulagrawal9@gmail.com>
2024-05-05 17:58:45 +0530

    Completed Navbar

--- config/site.js
+++ config/site.js
@@ -1,5 +1,6 @@
 const siteConfig = {
   name: 'Jira Clone 🚀',
+  title: 'Jira',
   description: 'An open source Jira clone built with Next.js 14, featuring React server components. Powered by tailwind, shadcn. Efficient project management at your fingertips.',
   keywords: ['Jira', 'Next.js', 'React', 'Tailwind CSS', 'Server Components', 'shadcn'],
   authors: [
